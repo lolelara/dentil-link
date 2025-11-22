babf45c793504b59b118b07404316a92242b25af
omnia <ob.charlie.21@gmail.com>
2025-11-22 04:00:12 +0200

    update message

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,6 +1,6 @@
 import { Client, Account, Databases } from 'appwrite';
 
-const client = new Client();
+export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
